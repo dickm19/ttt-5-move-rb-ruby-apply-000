edf0c334fa5ca352f7591040646c0fa1d506ad48
Mary Dick <maryclairedick@gmail.com>
2020-07-16 23:34:27 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -13,7 +13,7 @@ def input_to_index(user_input)
   converted_input = user_input - 1
 end
 
- def move(board, converted_input, character = X)
+ def move(board, converted_input, character = "X" )
    board[converted_input] = character
  end
  
